c3dc42af0370495f49f76855a914460bd4665011
Harpreet Singh <hurpreet@icloud.com>
2023-08-19 12:29:16 -0400

    Lab Completed

---
+++ lib/insert.sql
@@ -0,0 +1,31 @@
+INSERT INTO bears
+(name, age, sex, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 5, 'M', 'Brown',  'Empathetic', 1);
+
+INSERT INTO bears
+(name, age, sex, color, temperament, alive) 
+VALUES ('Rowdy', 5, 'M', 'Black', 'Angry', 1);
+
+INSERT INTO bears
+(name, age, sex, color, temperament, alive) 
+VALUES ('Tabitha', 7, 'F', 'White', 'joyful', 1);
+
+INSERT INTO bears
+(name, age, sex, color, temperament, alive) 
+VALUES ('Sergeant Brown', 11, 'M', 'Black', 'Angry', 1);
+
+INSERT INTO bears
+(name, age, sex, color, temperament, alive) 
+VALUES ('Melissa', 8, 'F', 'Brown', 'Playful', 1 );
+
+INSERT INTO bears
+(name, age, sex, color, temperament, alive) 
+VALUES ('Grinch', 4, 'M', 'Cinnamon', 'Fearful', 1 );
+
+INSERT INTO bears
+(name, age, sex, color, temperament, alive) 
+VALUES ('Wendy', 12, 'F', 'Blond', 'Social', 0);
+
+INSERT INTO bears
+(name, age, sex, color, temperament, alive) 
+VALUES (NULL, 11, 'F', 'Blue-Gray', 'Angry', 1);
